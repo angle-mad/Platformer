28bb4cadcf065ad5f28a023776d5fe645dcd8344
angle-mad <johndoe@example.com>
2025-10-19 13:15:28 -0700

    changed background, added double jump feature, added platforms, changed the collectable to be on a timer, collectable gives you ability to double jump, added more tiles to the seen, added labels to scene telling the controls

--- scripts/berry.gd
+++ scripts/berry.gd
@@ -1,7 +1,28 @@
 extends Sprite2D
 
+var counter: int = 0
+var respawn_timer: float = 0.0
+@onready var area_2d = get_parent()
+@onready var collision_shape = $"../CollisionShape2D"
 
-
+func _physics_process(delta: float) -> void:
+	if respawn_timer > 0:
+		respawn_timer -= delta
+		if respawn_timer <= 0:
+			visible = true
+			area_2d.visible = true
+			collision_shape.set_deferred("disabled", false)
+			counter = 0
 
 func _on_collectable_body_entered(_body):
-	queue_free() # Replace with function body.
+	counter += 1
+	if counter >= 1:
+		print(counter)
+		# Give the player double jump ability
+		if _body.has_method("unlock_double_jump"):
+			_body.unlock_double_jump()
+	
+	visible = false
+	area_2d.visible = false
+	collision_shape.set_deferred("disabled", true)
+	respawn_timer = 3.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var double_jump_available = false
+var double_jump_unlocked = false
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-
+	
+	# Only allow double jump if it's been unlocked
+	if double_jump_unlocked:
+		double_jump_available = true
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-
-
-
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif double_jump_unlocked and not is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			double_jump_unlocked = false
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
-	if direction > 0 :
+	if direction > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0 :
-			animated_sprite.flip_h = true
+	elif direction < 0:
+		animated_sprite.flip_h = true
 	
 	if is_on_floor():
 		if direction == 0:
@@ -40,5 +45,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+
+func unlock_double_jump():
+	double_jump_unlocked = true
