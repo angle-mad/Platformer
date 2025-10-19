eabb96ed199f2b0799b4972cc38f0814b81358fb
angle-mad <johndoe@example.com>
2025-10-19 04:13:59 -0700

    completely fail to implement wall jumps, changed background color, added jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -18,6 +18,7 @@ func _physics_process(delta: float) -> void:
 
 
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -27,9 +28,13 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0 :
 			animated_sprite.flip_h = true
 	
-	if direction == 0:
-		animated_sprite.play("idle")
-	else: animated_sprite.play("run")
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else: 
+			animated_sprite.play("run")
+	else: 
+		animated_sprite.play("jump")
 	
 	if direction:
 		velocity.x = direction * SPEED
